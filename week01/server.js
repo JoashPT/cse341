f30bf0a3312afe99658dc0187dc3b6cb243e6181
Joash Tantiangco <tan24001@byui.edu>
2025-03-07 17:00:21 +0800

    changed "start-node" to "start-prod" in package.json and PORT to port

--- week01/server.js
+++ week01/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send("Hello User");
 });
 
 const port = 3000;
 
-app.listen(process.env.PORT || 3000);
-console.log('Web Server is listening at port ' + (port));
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
