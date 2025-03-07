6fabe19b03eac4cbf4ae0d94d5d764b06370edcc
Joash Tantiangco <tan24001@byui.edu>
2025-03-07 16:53:24 +0800

    changed the message in console.log

--- week01/server.js
+++ week01/server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 const port = 3000;
 
 app.listen(process.env.PORT || 3000);
-console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+console.log('Web Server is listening at port ' + (port));
