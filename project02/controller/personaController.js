c8ea1b056c5362409da0ee2906771d60270c562a
Joash Tantiangco <tan24001@byui.edu>
2025-04-02 13:05:06 +0800

    updated error handling

--- project02/controller/personaController.js
+++ project02/controller/personaController.js
@@ -20,78 +20,115 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must enter a valid ID to find Persona.');
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error("Must enter a valid ID to find Persona.");
+        }
+        const personaId = new ObjectId(req.params.id);
+        const personaExist = await mongodb.getDatabase().db().collection('persona').findOne({ _id: new ObjectId(req.params.id) });
+        if(!personaExist) {
+            throw new Error("Must enter an existing ID to find Persona.");
+        }
+
+        mongodb
+            .getDatabase()
+            .db()
+            .collection('persona')
+            .find({_id: personaId})
+            .toArray()
+            .then((persona) => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(200).json(persona[0])
+            })
+            .catch((err) => {
+                if (err) {
+                    res.status(400).json({message: err});
+                }
+            })
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
-    const personaId = new ObjectId(req.params.id);
-    mongodb
-        .getDatabase()
-        .db()
-        .collection('persona')
-        .find({_id: personaId})
-        .toArray()
-        .then((persona) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(persona[0])
-        })
-        .catch((err) => {
-            if (err) {
-                res.status(400).json({message: err});
-            }
-        })
 }
 
 const createOne = async (req, res) => {
-    const personaData = {
-        persona: req.body.persona,
-        arcana: req.body.arcana,
-        level: req.body.level,
-        awakenInto: req.body.awakenInto,
-        firstAppearance: req.body.firstAppearance,
-        user: req.body.user,
-        denote: req.body.denote
-    };
-    const result = await mongodb.getDatabase().db().collection('persona').insertOne(personaData);
-    if (result.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'An error occurred while creating the contact.');
+    try {
+        const personaData = {
+            persona: req.body.persona,
+            arcana: req.body.arcana,
+            level: req.body.level,
+            awakenInto: req.body.awakenInto,
+            firstAppearance: req.body.firstAppearance,
+            user: req.body.user,
+            denote: req.body.denote
+        };
+        const result = await mongodb.getDatabase().db().collection('persona').insertOne(personaData);
+        if (result.acknowledged) {
+            res.status(204).send();
+        }
+    } catch(error) {
+        res.status(500).json(error || 'An error occurred while creating the contact.');
     }
 }
 
 const updateOne = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must enter a valid ID to update persona.");
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error("Must enter a valid ID to update Persona.");
+        }
+        
+        const personaExist = await mongodb.getDatabase().db().collection('persona').findOne({ _id: new ObjectId(req.params.id) });
+        if(!personaExist) {
+            throw new Error("Must enter an existing ID to update Persona.");
+        }
+
+        try {
+            const personaId = new ObjectId(req.params.id);
+            const personaData = {
+            persona: req.body.persona,
+            arcana: req.body.arcana,
+            level: req.body.level,
+            awakenInto: req.body.awakenInto,
+            firstAppearance: req.body.firstAppearance,
+            user: req.body.user,
+            denote: req.body.denote
+            };
+            const result = await mongodb.getDatabase().db().collection('persona').replaceOne({_id: personaId}, personaData);
+            if (result.modifiedCount > 0) {
+            res.status(204).send();
+            }
+        } catch (error) {
+            res.status(500).json(error || "An error occurred while updating a persona.");
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
-    const personaId = new ObjectId(req.params.id);
-    const personaData = {
-        persona: req.body.persona,
-        arcana: req.body.arcana,
-        level: req.body.level,
-        awakenInto: req.body.awakenInto,
-        firstAppearance: req.body.firstAppearance,
-        user: req.body.user,
-        denote: req.body.denote
-    };
-    const result = await mongodb.getDatabase().db().collection('persona').replaceOne({_id: personaId}, personaData);
-    if (result.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || "An error occurred while updating a persona.");
-    }
+    
 }
 
 const eraseOne = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must enter a valid ID to delete a persona entry.");
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error("Must enter a valid ID to delete a persona entry.");
+        }
+
+        const personaExist = await mongodb.getDatabase().db().collection('persona').findOne({ _id: new ObjectId(req.params.id) });
+        if(!personaExist) {
+            throw new Error("Must enter an existing ID to delete Persona.");
+        }
+
+        try {
+            const personaId = new ObjectId(req.params.id);
+            const result = await mongodb.getDatabase().db().collection('persona').deleteOne({_id: personaId});
+            if (result.deletedCount > 0) {
+                res.status(204).send();
+            }
+        } catch (error) {
+            res.status(500).json(result.error || "An error occurred while deleting a persona.");
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
-    const personaId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('persona').deleteOne({_id: personaId});
-    if (result.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || "An error occurred while deleting a persona.");
-    }
+   
 }
 
 module.exports = {
--- project02/controller/pokeController.js
+++ project02/controller/pokeController.js
@@ -20,77 +20,113 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must enter a valid ID to find Pokemon.');
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error('Must enter a valid ID to find Pokemon.');
+        }
+        
+        const pokemonId = new ObjectId(req.params.id);
+        const pokemonExist = await mongodb.getDatabase().db().collection('pokemon').findOne({ _id: new ObjectId(req.params.id) });
+                if(!pokemonExist) {
+                    throw new Error("Must enter an existing ID to find Pokemon.");
+                }
+
+        mongodb
+            .getDatabase()
+            .db()
+            .collection('pokemon')
+            .find({_id: pokemonId})
+            .toArray()
+            .then((pokemon) => {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(pokemon[0])
+            })
+            .catch((err) => {
+                if (err) {
+                    res.status(400).json({message: err});
+                }
+            })
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
-    const pokemonId = new ObjectId(req.params.id);
-    mongodb
-        .getDatabase()
-        .db()
-        .collection('pokemon')
-        .find({_id: pokemonId})
-        .toArray()
-        .then((pokemon) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(pokemon[0])
-        })
-        .catch((err) => {
-            if (err) {
-                res.status(400).json({message: err});
-            }
-        })
 }
 
 const createOne = async (req, res) => {
-    const pokeData = {
-        pokedexNo: req.body.pokedexNo,
-        name: req.body.name,
-        nickname: req.body.nickname,
-        level: req.body.level,
-        nature: req.body.nature,
-        heldItem: req.body.heldItem,
-        originalTrainer: req.body.originalTrainer
-    };
-    const result = await mongodb.getDatabase().db().collection('pokemon').insertOne(pokeData);
-    if (result.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'An error occurred while creating the contact.');
+    try {
+        const pokeData = {
+            pokedexNo: req.body.pokedexNo,
+            name: req.body.name,
+            nickname: req.body.nickname,
+            level: req.body.level,
+            nature: req.body.nature,
+            heldItem: req.body.heldItem,
+            originalTrainer: req.body.originalTrainer
+        };
+        const result = await mongodb.getDatabase().db().collection('pokemon').insertOne(pokeData);
+        if (result.acknowledged) {
+            res.status(204).send();
+        }
+    } catch (error) {
+        res.status(500).json(error || 'An error occurred while creating the contact.');
     }
 }
 
 const updateOne = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must enter a valid ID to update Pokemon.");
-    }
-    const pokemonId = new ObjectId(req.params.id);
-    const pokeData = {
-        pokedexNo: req.body.pokedexNo,
-        name: req.body.name,
-        nickname: req.body.nickname,
-        level: req.body.level,
-        nature: req.body.nature,
-        heldItem: req.body.heldItem,
-        originalTrainer: req.body.originalTrainer
-    };
-    const result = await mongodb.getDatabase().db().collection('pokemon').replaceOne({_id: pokemonId}, pokeData);
-    if (result.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || "An error occurred while updating a Pokemon.");
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error("Must enter a valid ID to update Pokemon.");
+        }
+
+        const pokemonExist = await mongodb.getDatabase().db().collection('pokemon').findOne({ _id: new ObjectId(req.params.id) });
+            if(!pokemonExist) {
+                throw new Error("Must enter an existing ID to find Pokemon.");
+            }
+        
+        try {
+            const pokemonId = new ObjectId(req.params.id);
+            const pokeData = {
+            pokedexNo: req.body.pokedexNo,
+            name: req.body.name,
+            nickname: req.body.nickname,
+            level: req.body.level,
+            nature: req.body.nature,
+            heldItem: req.body.heldItem,
+            originalTrainer: req.body.originalTrainer
+            };
+            const result = await mongodb.getDatabase().db().collection('pokemon').replaceOne({_id: pokemonId}, pokeData);
+            if (result.modifiedCount > 0) {
+                res.status(204).send();
+            }
+        } catch (error) {
+            res.status(500).json(error || "An error occurred while updating a pokemon.");
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
 }
 
 const eraseOne = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must enter a valid ID to delete a Pokemon entry.");
-    }
-    const pokemonId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('pokemon').deleteOne({_id: pokemonId});
-    if (result.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || "An error occurred while deleting a Pokemon.");
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            throw new Error("Must enter a valid ID to delete a Pokemon entry.");
+        }
+
+        const pokemonExist = await mongodb.getDatabase().db().collection('pokemon').findOne({ _id: new ObjectId(req.params.id) });
+        if(!pokemonExist) {
+            throw new Error("Must enter an existing ID to delete a Pokemon.");
+        }
+
+        try {
+            const pokemonId = new ObjectId(req.params.id);
+            const result = await mongodb.getDatabase().db().collection('pokemon').deleteOne({_id: pokemonId});
+            if (result.deletedCount > 0) {
+                res.status(204).send();
+            }
+        } catch (error) {
+            res.status(500).json(result.error || "An error occurred while deleting a pokemon.");
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
 }
 
